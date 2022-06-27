159106203f856433b73594a7a22576591d32f5e6
sm-amoled <moktak072@naver.com>
2022-06-28 01:47:26 +0900

    🎉 Watch Target 추가

---
+++ posmealWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  posmealWatch WatchKit Extension
+//
+//  Created by Park Sungmin on 2022/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ posmealWatch WatchKit Extension/_______App.swift
@@ -0,0 +1,19 @@
+//
+//  _______App.swift
+//  posmealWatch WatchKit Extension
+//
+//  Created by Park Sungmin on 2022/06/28.
+//
+
+import SwiftUI
+
+@main
+struct _______App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
