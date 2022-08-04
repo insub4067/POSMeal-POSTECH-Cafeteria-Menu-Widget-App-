8b3391350ba02b641451cbfd4eba00a9627fbaa2
sm-amoled <moktak072@naver.com>
2022-08-05 01:40:04 +0900

    Complication 추가
    WatchOS 아이콘 추가
    Complication 이미지 추가
    circularSmall, graphicCorner, modularSmall에 대한 complication 추가

--- posmealWatch WatchKit Extension/ComplicationController.swift
+++ posmealWatch WatchKit Extension/ComplicationController.swift
@@ -22,8 +22,9 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
             CLKComplicationDescriptor(identifier: "complication", displayName: "포스밀", supportedFamilies: [
                 .circularSmall,
                 .graphicCorner,
-                .modularLarge,
-                .graphicRectangular
+                .modularSmall,
+//                .modularLarge,
+//                .graphicRectangular
             ])
             // Multiple complication support can be added here with more descriptors
         ]
@@ -53,53 +54,103 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline 
         switch complication.family {
-        case .modularLarge:
-            let dateComponent = Calendar.current.dateComponents([.month, .day, .hour, .minute, .second], from: Date())
-            var menu: [String] = []
-            var restau_name = ""
             
-            if 0 <= dateComponent.hour! && dateComponent.hour! < 9 {
-                menu = network.BREAKFAST_A
-                restau_name = "조식"
-            }
-            else if 9 <= dateComponent.hour! && dateComponent.hour! < 14 {
-                menu = network.LUNCH
-                restau_name = "중식"
-            }
-            else if 14 <= dateComponent.hour! && dateComponent.hour! <= 23 {
-                menu = network.DINNER
-                restau_name = "석식"
-            }
+        case .modularSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "포스밀_글자_40.png")!)
+            let template = CLKComplicationTemplateModularSmallSimpleImage(imageProvider: imageProvider)
+
+            handler(CLKComplicationTimelineEntry(date:Date(),
+                                                 complicationTemplate: template))
             
-            if menu.isEmpty {
-                handler(nil)
-                return
-            }
-            
-            guard let template = makeTemplate(title: "\(dateComponent.month!)월 \(dateComponent.day!)일 \(restau_name)", body: "\(menu[0])\n\(menu[1]) 외 \(menu.count-2 == 0 ? "" : String(menu.count-2))", comlication: complication)
-            else {
-                handler(nil)
-                return
-            }
-            
-            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
-            
-            handler(entry)
-        
             
         case .graphicCorner:
-            let imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "포스밀_20.png")!)
+            let imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "포스밀_글자_40.png")!)
             let template = CLKComplicationTemplateGraphicCornerCircularImage(imageProvider: imageProvider)
             
             handler(CLKComplicationTimelineEntry(date:Date(),
                                                  complicationTemplate: template))
             
+            
         case .circularSmall:
-            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "포스밀_20.png")!)
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "포스밀_글자_40.png")!)
             let template = CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: imageProvider)
 
             handler(CLKComplicationTimelineEntry(date: Date()
                                                  , complicationTemplate: template))
+            
+            
+//        case .modularLarge:
+//            let dateComponent = Calendar.current.dateComponents([.month, .day, .hour, .minute, .second], from: Date())
+//            var menu: [String] = []
+//            var restau_name = ""
+//
+//            if 0 <= dateComponent.hour! && dateComponent.hour! < 9 {
+//                menu = network.BREAKFAST_A
+//                restau_name = "조식"
+//            }
+//            else if 9 <= dateComponent.hour! && dateComponent.hour! < 14 {
+//                menu = network.LUNCH
+//                restau_name = "중식"
+//            }
+//            else if 14 <= dateComponent.hour! && dateComponent.hour! <= 23 {
+//                menu = network.DINNER
+//                restau_name = "석식"
+//            }
+//
+//            if menu.isEmpty {
+//                let entry: CLKComplicationTimelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: CLKComplicationTemplateModularLargeStandardBody(headerTextProvider: CLKTextProvider(format: "포스밀"),
+//                                                                                                                                                                           body1TextProvider: CLKSimpleTextProvider(text: "메뉴가 없습니다"),
+//                                                                                                                                                                           body2TextProvider: CLKSimpleTextProvider(text: "")))
+//
+//                handler(entry)
+//                return
+//            }
+//
+//            guard let template = makeTemplate(title: "\(dateComponent.month!)월 \(dateComponent.day!)일 \(restau_name)", body: "\(menu[0])\n\(menu[1]) 외 \(menu.count-2 == 0 ? "" : String(menu.count-2))", comlication: complication)
+//            else {
+//                handler(nil)
+//                return
+//            }
+//
+//            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+//
+//            handler(entry)
+//
+//        case .graphicRectangular:
+//            let dateComponent = Calendar.current.dateComponents([.month, .day, .hour, .minute, .second], from: Date())
+//            var menu: [String] = []
+//            var restau_name = ""
+//
+//            if 0 <= dateComponent.hour! && dateComponent.hour! < 9 {
+//                menu = network.BREAKFAST_A
+//                restau_name = "조식"
+//            }
+//            else if 9 <= dateComponent.hour! && dateComponent.hour! < 14 {
+//                menu = network.LUNCH
+//                restau_name = "중식"
+//            }
+//            else if 14 <= dateComponent.hour! && dateComponent.hour! <= 23 {
+//                menu = network.DINNER
+//                restau_name = "석식"
+//            }
+//
+//            if menu.isEmpty {
+//                let entry: CLKComplicationTimelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: CLKComplicationTemplateGraphicRectangularLargeView(headerTextProvider: CLKSimpleTextProvider(text: "포스밀"), content: ComplicationModularLargeView(text: "메뉴가 없습니다")))
+//
+//                handler(entry)
+//                return
+//            }
+//
+//            guard let template = makeTemplate(title: "\(dateComponent.month!)월 \(dateComponent.day!)일 \(restau_name)", body: "\(menu[0])\n\(menu[1]) 외 \(menu.count-2 == 0 ? "" : String(menu.count-2))", comlication: complication)
+//            else {
+//                handler(nil)
+//                return
+//            }
+//
+//            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+//
+//            handler(entry)
+
             
         default:
             handler(nil)
@@ -124,17 +175,21 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         
             handler(template)
             
+        case .modularSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "포스밀_글자_40.png")!)
+            handler(CLKComplicationTemplateModularSmallSimpleImage(imageProvider: imageProvider))
+            
         case .graphicRectangular:
             let headerTextProvider = CLKSimpleTextProvider(text: "포스밀")
             let bodyTextProvider = CLKSimpleTextProvider(text: "오늘 식단이 여기에 표시됩니다")
             handler(CLKComplicationTemplateGraphicRectangularStandardBody(headerTextProvider: headerTextProvider, body1TextProvider: bodyTextProvider))
             
         case .graphicCorner:
-            let imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "포스밀_20.png")!)
+            let imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "포스밀_글자_40.png")!)
             handler(CLKComplicationTemplateGraphicCornerCircularImage(imageProvider: imageProvider))
             
         case .circularSmall:
-            let  imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "포스밀_20.png")!)
+            let  imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "포스밀_글자_40.png")!)
             handler(CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: imageProvider))
             
         default:
@@ -157,12 +212,14 @@ extension ComplicationController {
                                                                       content: ComplicationModularLargeView(text: body))
             
         case .circularSmall:
-            return CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: CLKImageProvider(onePieceImage: UIImage(systemName: "heart.text.square")!))
+            return CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: CLKImageProvider(onePieceImage: UIImage(named:  "포스밀_40.png")!))
             
         case .graphicCorner:
 //            return CLKComplicationTemplateModularSmallSimpleText(textProvider: CLKTextProvider(format: title, []))
             return CLKComplicationTemplateGraphicCornerTextView(textProvider: CLKTextProvider(format: title, []), label: Text("Text"))
             
+        case .modularSmall:
+            return CLKComplicationTemplateModularSmallSimpleImage(imageProvider: CLKImageProvider(onePieceImage: UIImage(named: "포스밀_32.png")!))
             
             //shortcutProvider.getShortcutComplication())
         default:
